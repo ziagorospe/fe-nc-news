de2818a259f88fd619fe6ca392a7ef4cea0597f4
ziagorospe <ziagorospe@gmail.com>
2024-03-12 11:57:08 +0000

    added cuntionality to vote on articles and provide feedback to the user when voting not possible, optimistic rendering for number of votes

--- src/components/Articles/ArticleList.jsx
+++ src/components/Articles/ArticleList.jsx
@@ -1,19 +1,46 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import FocusArticleContext from '../../contexts/FocusArticle'
+import axios from "axios";
 
 function ArticleList(props){
     const {setFocusArticle} = useContext(FocusArticleContext)
+    const {articleList} = props
+    const [voteList, setVoteList] = useState(articleList.map((article)=>{
+        return article.votes
+    }))
+    const [voteErrorMsg, setVoteErrorMsg] = useState("")
+
     function setSelectedArticle(event){
         setFocusArticle(event.target.attributes[0].value)
     }
 
-    
+    function updateVote(event){
+        setVoteErrorMsg("")
+        const voteMod = event.target.value*1
+        const articleId = event.target.id
+        const voteIndex = event.target.attributes.index.value*1
+        const tempVote = [...voteList]
+        tempVote[voteIndex] += voteMod
+        setVoteList(tempVote)
+        
+         axios.patch(`https://backend-nc-news-project.onrender.com/api/articles/${articleId}`,{
+             inc_votes: voteMod
+         })
+         .then((response)=>{
+            
+         })
+         .catch(()=>{
+            const tempVote = [...voteList]
+            tempVote[voteIndex] -= voteMod
+            setVoteList(tempVote)
+         })
 
-    const {articleList} = props
+    }
+
     return <>
         {
-            articleList.map((article)=>{
+            articleList.map((article, index)=>{
                 return (<article key={article.article_id}>
                     <img src={article.article_img_url} alt={article.title} style={{width:200+'px'}} />
                     <h1>Title: {article.title}</h1>
@@ -22,9 +49,10 @@ function ArticleList(props){
                     <h3>Date {article.created_at}</h3>
                     <div>
                         
-                        <button value="1">+</button>
-                        <h4>Votes: {article.votes}</h4>
-                        <button value="-1">-</button>
+                        <button onClick={updateVote} value="1" id={article.article_id} index={index}>+</button>
+                        {voteErrorMsg ? <p>couldn't vote</p> : <></>}
+                        <h4>Votes: {voteList[index]}</h4>
+                        <button onClick={updateVote} value="-1" id={article.article_id} index={index}>-</button>
                     </div>
                     <Link to="/article" >
                         <h4 onClick={setSelectedArticle} value={article.article_id}>Comments: {article.comment_count}</h4>
