d663846658dd001c562bbf54e252fa36b07d9cb3
ziagorospe <ziagorospe@gmail.com>
2024-03-13 11:16:08 +0000

    added functionality to view articles by category with the ability to make the request in the URL

--- src/components/Home/SearchBar.jsx
+++ src/components/Home/SearchBar.jsx
@@ -1,48 +1,62 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useSearchParams } from 'react-router-dom';
 
-function SearchBar(){
+function SearchBar(props){
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [categoryList, setCategoryList] = useState([])
+  const {currentCategory} = props
+  const {setCurrentCategory} = props
 
-    const [categoryList, setCategoryList] = useState([])
+  useEffect(()=>{
+      axios.get('https://backend-nc-news-project.onrender.com/api/topics')
+      .then((response)=>{
+          setCategoryList([...response.data.topics])
+      })
+  },[])
 
-    useEffect(()=>{
-        axios.get('https://backend-nc-news-project.onrender.com/api/topics')
-        .then((response)=>{
-            setCategoryList([...response.data.topics])
-        })
-    },[])
 
-    return (
-        <form>
-            <div>
-                <select name="Sort By">
-                    <option value="">Sort By</option>
-                    <option value="created_at">Latest</option>
-                    <option value="title">Title</option>
-                    <option value="votes">Votes</option>
-                    <option value="comment_count">Comments</option>
-                </select>
-                <select name="Order By">
-                    <option value="desc">Descending</option>
-                    <option value="asc">Ascending</option>
-                </select>
-            </div>
-            <div>
-        <input type="text" />
-        <select name="Categories" id="">
-          <option value="">All Categories</option>
-          {categoryList.map((category) => {
-            return (
-              <option key={category.slug} value={category.slug}>
-                {category.slug}
-              </option>
-            );
-          })}
-        </select>
-        <button type="submit">Search</button>
-      </div>
-        </form>
-    )
+  function changeCategory(event){
+    setCurrentCategory(event.target.value)
+    if(event.target.value){
+      setSearchParams({topic: event.target.value}) 
+    } else {
+      setSearchParams("")
+    }
+    
+  }
+
+  return (
+      <form>
+          <div>
+              <select name="Sort By">
+                  <option value="">Sort By</option>
+                  <option value="created_at">Latest</option>
+                  <option value="title">Title</option>
+                  <option value="votes">Votes</option>
+                  <option value="comment_count">Comments</option>
+              </select>
+              <select name="Order By">
+                  <option value="desc">Descending</option>
+                  <option value="asc">Ascending</option>
+              </select>
+          </div>
+          <div>
+      <input type="text" />
+      <select value={currentCategory} onChange={changeCategory} name="Categories" id="">
+        <option value="">All Categories</option>
+        {categoryList.map((category) => {
+          return (
+            <option key={category.slug} value={category.slug}>
+              {category.slug}
+            </option>
+          );
+        })}
+      </select>
+      <button type="submit">Search</button>
+    </div>
+      </form>
+  )
 }
 
 export default SearchBar;
